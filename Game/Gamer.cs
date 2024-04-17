531852fd0fe0419498b3aa1a53280817d5b8f288
lanoth <78845859+lanoth@users.noreply.github.com>
2024-04-17 10:28:29 +0200

    Added cards

---
+++ Game/Gamer.cs
@@ -0,0 +1,8 @@
+using Xyaneon.Games.Cards.StandardPlayingCards;
+
+namespace Game;
+
+public class Gamer
+{
+    public List<StandardPlayingCard> Hand = [];
+}
---
+++ Game/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Game;
+
+List<Gamer> gamers = [];
+
+gamers.Add(new Gamer());
+gamers.Add(new Gamer());
+
+Cards.deck.Shuffle();
+
+Cards.Deal(gamers);
+
+foreach (var gamer in gamers)
+{
+    Console.WriteLine("-----");
+    foreach (var card in gamer.Hand)
+    {
+        Console.WriteLine($"{card.Rank} of {card.Suit}");
+    }
+}
