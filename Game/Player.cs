0b87a06ebb7bff44d3222232803554275a26f68f
NikolajRFA <nrfadk@gmail.com>
2024-04-17 13:40:23 +0200

    Created Player.Take and tests

--- Game/Player.cs
+++ Game/Player.cs
@@ -1,6 +1,25 @@
-﻿namespace Game;
+﻿using Xyaneon.Games.Cards.StandardPlayingCards;
+
+namespace Game;
 
 public class Player
 {
-    
+    public string Name { get; set; }
+    public List<StandardPlayingCard> Hand { get; set; } = [];
+    public Dictionary<StandardPlayingCard, bool> PointPile { get; set; } = [];
+
+    public bool Take(Table table, int index, StandardPlayingCard card)
+    {
+        if (table.Cards[index].Value.Any(x => GameState.CardToValue(card).Any(y => x == y)))
+        {
+            foreach (var tableCard in table.Cards[index].Key.Cards)
+            {
+                PointPile.Add(tableCard, table.Cards.Count == 1);
+            }
+
+            return true;
+        }
+
+        return false;
+    }
 }
---
+++ GameTests/PlayerTests.cs
@@ -0,0 +1,27 @@
+﻿using Game;
+using Xyaneon.Games.Cards.StandardPlayingCards;
+
+namespace TestProject1;
+
+public class PlayerTests
+{
+    [Fact]
+    public void Take_Table5WithHand5_True()
+    {
+        var table = new Table();
+        table.AddCard(new StandardPlayingCard(Rank.Five, Suit.Hearts));
+        var player = new Player { Name = "Alex" };
+        
+        Assert.True(player.Take(table, 0, new StandardPlayingCard(Rank.Five, Suit.Diamonds)));
+    }
+
+    [Fact]
+    public void Take_Table5WithHand6_False()
+    {
+        var table = new Table();
+        table.AddCard(new StandardPlayingCard(Rank.Five, Suit.Hearts));
+        var player = new Player { Name = "Alex" };
+        
+        Assert.False(player.Take(table, 0, new StandardPlayingCard(Rank.Six, Suit.Diamonds)));
+    }
+}
