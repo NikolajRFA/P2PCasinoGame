c0a852dbe1212d54188a5af37d243d2acc755188
lanoth <78845859+lanoth@users.noreply.github.com>
2024-04-17 14:00:10 +0200

    Removed unused classes

--- Game/Gamer.cs
+++
@@ -1,8 +0,0 @@
-using Xyaneon.Games.Cards.StandardPlayingCards;
-
-namespace Game;
-
-public class Gamer
-{
-    public List<StandardPlayingCard> Hand = [];
-}
--- Game/Program.cs
+++ Game/Program.cs
@@ -1,20 +1,3 @@
-﻿
-using Game;
+﻿// test
 
-List<Gamer> gamers = [];
-
-gamers.Add(new Gamer());
-gamers.Add(new Gamer());
-
-Cards.deck.Shuffle();
-
-Cards.Deal(gamers);
-
-foreach (var gamer in gamers)
-{
-    Console.WriteLine("-----");
-    foreach (var card in gamer.Hand)
-    {
-        Console.WriteLine($"{card.Rank} of {card.Suit}");
-    }
-}
+Console.WriteLine("");
