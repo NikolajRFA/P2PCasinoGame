5176cb6b5914a3f676d5bc8938c6c97d2a3b4ef4
lanoth <78845859+lanoth@users.noreply.github.com>
2024-04-18 10:44:20 +0200

    Added SumPoints test

--- GameTests/GameStateTests.cs
+++ GameTests/GameStateTests.cs
@@ -1,14 +1,31 @@
 using Game;
+using Xunit.Abstractions;
+using Xyaneon.Games.Cards.StandardPlayingCards;
 
 namespace TestProject1;
 
 public class GameStateTests
 {
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public GameStateTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     [Fact]
     public void Constructor_3Players_3PlayersReceive4CardsEachAndTableHas4Cards()
     {
         var gameState = new GameState(["Alex", "Nikolaj", "Laust"]);
         gameState.Players.ForEach(player => Assert.True(player.Hand.Count == 4));
         Assert.True(gameState.Table.Cards.Count == 4);
+    }
+
+    [Fact]
+    public void SumPoints_AllCardsToOnePlayer_11()
+    {
+        var gameState = new GameState(["Alex"]);
+        gameState.Players[0].PointPile.AddRange(new StandardPlayingCardDeck().Cards);
+        Assert.Equal(11, gameState.SumPoints()[0].Item2);
     }
 }
